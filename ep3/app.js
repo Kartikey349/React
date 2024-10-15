2e42f40dbf8ed0588f31dfeee9f341a1d83b9617
kartikey sharma <kartikey7518@gmail.com>
2024-10-15 18:42:03 +0530

    functional component

--- ep3/app.js
+++ ep3/app.js
@@ -10,6 +10,12 @@ const jsxHeading = <h1 id = "heading">Namaste React using JSX</h1>
 console.log(heading);
 console.log(jsxHeading)
 
+const HeadingComponent = () => (
+    <div id="container">
+        <h1 className="heading">functional Component</h1>;
+    </div> 
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
