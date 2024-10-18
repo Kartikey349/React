13d7726b6afb28b5cd63580f71f29670af7e77d2
kartikey sharma <kartikey7518@gmail.com>
2024-10-18 12:04:46 +0530

    assignment practice

--- ep3/app.js
+++ ep3/app.js
@@ -1,3 +1,4 @@
+import react from "react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
@@ -23,6 +24,42 @@ const HeadingComponent = () => (
         <h1 className="heading">functional Component</h1>
     </div> 
 );
+
+// const heading = React.createElement('div', {id: "heading"},
+//     [
+//         React.createElement('h1',{}, "hello there"),
+//         React.createElement('h1',{}, "hello there"),
+//         React.createElement('h1',{}, "hello there")
+//     ]
+// )
+
+// const heading = (
+//     <div id = "main">
+//         <h1 id = "heading">hello there from jsx</h1>
+//         <h1>hello there from jsx</h1>
+//         <h1>hello there from jsx</h1>
+//     </div>
+// );
+
+// const Heading = () => (
+//     <div id = "main">
+//         {heading}
+//          <h1>hello there</h1>
+//          <h1>hello there</h1>
+//          <h1>hello there</h1>
+//      </div>
+// )
+
+// const Header = () => (
+//     <div id = "header">
+//         <nav>
+//             <img src="icon.png" />
+//             <input type="text" placeholder = 
+//             "type something" />
+//             <img src="./profile.png" alt="" className="img-profile" />
+//         </nav>
+//     </div>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
