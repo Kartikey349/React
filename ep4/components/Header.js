6c8b615011db47b919ea3615c07a040e7899912a
kartikey sharma <kartikey7518@gmail.com>
2024-11-12 13:22:57 +0530

    made login-logout button using useState

--- ep4/components/Header.js
+++ ep4/components/Header.js
@@ -1,6 +1,10 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+
+    const [btnNameReact, setBtnName] = useState("Login");
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -13,6 +17,10 @@ const Header = () => {
                     <li>About</li>
                     <li>Contact</li>
                     <li>Cart</li>
+                    <button onClick={() => { 
+                       btnNameReact ==="Login" ? setBtnName("Logout") : setBtnName("Login");
+                    }
+                } className="login-btn">{btnNameReact}</button>
                 </ul>
             </div>
         </div>
