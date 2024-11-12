6425bffcdc71e6a856dbfc675c9ef8a17c7c73f8
kartikey sharma <kartikey7518@gmail.com>
2024-11-12 12:36:08 +0530

    generated card by direct data from swiggy API using useEffect

--- ep4/components/Body.js
+++ ep4/components/Body.js
@@ -1,18 +1,53 @@
 import RestaurentCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+// import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
+import Shimmer from "./shimmer";
 
- 
+// function filterData(searchText, listOfRestaurant){
+//     const filterData = listOfRestaurant.filter((restaurant) => restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
+// );
+//     return filterData;
+//  }
 
 const Body = () => {
+    // const [searchText, setSearchText] = useState("")
+    const [listOfRestaurant, setListOfRestaurant] = useState([]); 
 
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList); 
+useEffect(() => {
+    fetchApi();
+    }, []);
+
+
+async function fetchApi(){
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+    const json =  await data.json();
+
+    setListOfRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+ }
+     
+//conditional rendering
+ if(listOfRestaurant.length === 0){
+    return <Shimmer />;
+ }
 
     return(<div className="body">
+
+        {/* <div className="search">
+            <input id="search-bar" type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+
+            <button className="search-btn" onClick={() => {
+                const data = filterData(searchText, listOfRestaurant);
+
+                setListOfRestaurant(data);
+            }}>search</button>
+
+        </div> */}
+
         <div className="filter">
             <button className="filter-btn" 
             onClick={() => {
-              const filteredList = listOfRestaurant.filter((restaurant) =>  restaurant.data.avgRating > 4
+              const filteredList = listOfRestaurant.filter((restaurant) =>  restaurant.info.avgRating > 4.1
                 );
                 setListOfRestaurant(filteredList);
             }}>
@@ -22,7 +57,8 @@ const Body = () => {
 
         <div className="res-container">
            {
-            listOfRestaurant.map((restaurant) => (<RestaurentCard key={restaurant.data.id} resData = {restaurant} /> ))
+            listOfRestaurant.map((restaurant) => (
+                <RestaurentCard key={restaurant.info.id} resData={restaurant} />))
            }
         </div>
     </div>)
--- ep4/components/RestaurantCard.js
+++ ep4/components/RestaurantCard.js
@@ -14,9 +14,10 @@ const RestaurentCard = (props) => {
         name, 
         cuisines, 
         avgRating, 
-        costForTwo, 
-        deliveryTime
-    } = resData.data;
+        costForTwo,
+        sla,
+        slaString
+    } = resData.info;
 
     return(
         <div className="res-card">
@@ -24,8 +25,8 @@ const RestaurentCard = (props) => {
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating} stars</h4>
-            <h4>{costForTwo / 100}</h4>
-            <h4>{deliveryTime}</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{sla.slaString}</h4>
         </div>
     )
 }
