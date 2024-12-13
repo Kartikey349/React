392722dafdcce85b45aa01a81056dde2a5b5ca1f
kartikey sharma <kartikey7518@gmail.com>
2024-12-14 01:59:05 +0530

    added accordion feature to the Restaurant Category section

--- ep4/components/RestaurantCategory.js
+++ ep4/components/RestaurantCategory.js
@@ -1,15 +1,32 @@
+import { useState } from "react"
 import { ItemList } from "./ItemList"
 
 export const RestaurantCategory = ({data}) =>{
+
+    const [showItem, setShowItem] = useState(false)
+
+    const handleClick = () =>{
+        // if(showItem === false){
+        //     setShowItem(true);
+        // }else{
+        //     setShowItem(false);
+        // }
+
+        setShowItem(!showItem);
+    }
+
     return (
         <div>
             <div  className="w-6/12 bg-gray-100 mx-auto my-4 shadow-md p-4 ">
-            <div className="flex justify-between">
-                <span className="font-bold">{data.title} ({data.itemCards.length})</span>
-                <span>⬇️</span>
-            </div>
+                <div className="flex justify-between cursor-pointer" onClick={handleClick}>
 
-            <ItemList items={data.itemCards} />
+                    <span className="font-bold">{data.title} ({data.itemCards.length})</span>
+
+                    <span><i className="fa-solid fa-arrow-down"></i></span>
+                    
+                </div>
+
+                {showItem && <ItemList items={data.itemCards} />}
            </div>
         </div>
     )
