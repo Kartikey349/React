6648ad516be9dbd223329df49a1def9f87d267af
kartikey sharma <kartikey7518@gmail.com>
2024-12-14 01:30:30 +0530

    added items to the category inside the cardMenu

---
+++ ep4/components/ItemList.js
@@ -0,0 +1,31 @@
+import { CDN_URL } from "../utils/constants"
+
+export const ItemList = ({items}) => {
+     console.log(items)
+    return(
+        <div>
+           {items.map((item)=> {
+            return (
+                <div key={item.card.info.id} className="flex justify-between p-2 m-1 border-b-2 text-left items-center">
+                    <div className=" w-9/12">
+                    <div className="py-2">
+                        <span>{item.card.info.name}</span>
+                        <span> - ₹{item.card.info.price / 100 || item.card.info.defaultPrice / 100}</span>
+                    </div>   
+                    <p className="text-xs">{item.card.info.description}</p>
+                    </div>
+
+                <div className="w-3/12 p-4 relative">
+                    <div className="absolute bottom-0 ">
+                        <button className="bg-black text-white shadow-lg text-sm rounded-sm p-[2px] ">Add+</button>
+                    </div>
+
+                    <img src= {CDN_URL + item.card.info.imageId} className=" w-full"
+                    />
+                </div>
+                </div>
+            )
+           })} 
+        </div>
+    )
+}
--- ep4/components/shimmer.js
+++ ep4/components/shimmer.js
@@ -1,15 +1,18 @@
 const Shimmer = () => {
-    return (<div className="shimmer-container">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
+    return (
+    <div className="shimmer-container flex flex-wrap justify-center mt-[120px]">
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div> <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
+        <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] bg-gray-100 rounded-sm "></div>
     </div>);
 }
 
-export default Shimmer
+export default Shimmer;
